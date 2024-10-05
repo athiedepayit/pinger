4063ff55ed7ed1ea0fdf584b09c446a80f3788eb
adamthiede <me@adamthiede.com>
2024-10-05 09:57:25 -0500

    add check.go from gomon

---
+++ check.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "errors"
+    "net"
+    "time"
+)
+
+const timeout = time.Second*3
+
+func CheckTcp(host string, port string) (bool, error) {
+	// these values should be configurable
+	defaultPort := "22"
+
+	if port == "" {
+		port = defaultPort
+	}
+
+	hostToCheck := host + ":" + port
+
+	d := net.Dialer{Timeout: timeout}
+	conn, err := d.Dial("tcp", hostToCheck)
+	if err != nil {
+		dialError := fmt.Sprintf("Could not connect to port: %s:%s", host, port)
+		return false, errors.New(dialError)
+	}
+
+	conn.Close()
+
+	return true, nil
+}
